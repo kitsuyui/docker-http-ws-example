c58dbed34efc953d90e34f63703c8bde4aa6afcb
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-05-14 23:00:58 +0900

    Handle WebSocket connection errors

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ const main = () => {
   const wsServer = new WebSocketServer({ server });
 
   wsServer.on("connection", (ws) => {
+    ws.on("error", (error) => {
+      console.error(`WebSocket error: ${error.message}`);
+    });
+
     console.error("Sending: PING");
     ws.send("PING");
 
